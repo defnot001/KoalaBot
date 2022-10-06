1c8cea98312bf0e05068179ff75454ca0e11ce49
defnot001 <marc.miller994@gmail.com>
2022-10-06 19:47:58 +0200

    fixed syntax error

--- src/util/deploy-commands.js
+++ src/util/deploy-commands.js
@@ -2,18 +2,19 @@ import fs from 'fs';
 import path from 'path';
 import { REST } from '@discordjs/rest';
 import { Routes } from 'discord.js';
+import sourcePath from './node/sourcesPath.js';
 import { botEnv } from '../config/environment.js';
 
 const commands = [];
-const commandsPath = path.join(__dirname, 'commands');
+const commandsPath = path.join(sourcePath, 'commands');
 const commandFiles = fs
   .readdirSync(commandsPath)
   .filter((file) => file.endsWith('.js'));
 
 for (const file of commandFiles) {
   const filePath = path.join(commandsPath, file);
-  const command = require(filePath);
-  commands.push(command.data.toJSON());
+  const command = await import(filePath);
+  commands.push(command.command.data.toJSON());
 }
 
 const rest = new REST({ version: '10' }).setToken(botEnv.token);
