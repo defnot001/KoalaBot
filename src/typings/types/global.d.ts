3f111d40bfff64631e44717b3160f398408f910e
defnot001 <marc.miller994@gmail.com>
2022-11-21 23:05:50 +0100

    extended process.env

--- src/typings/types/global.d.ts
+++ src/typings/types/global.d.ts
@@ -4,6 +4,13 @@ declare global {
       BOT_TOKEN: string;
       CLIENT_ID: string;
       GUILD_ID: string;
+      URL: string;
+      API_KEY: string;
+      SMP_RCON_PASSWORD: string;
+      CMP_RCON_PASSWORD: string;
+      CMP2_RCON_PASSWORD: string;
+      COPY_RCON_PASSWORD: string;
+      SNAPSHOTS_RCON_PASSWORD: string;
     }
   }
 }
