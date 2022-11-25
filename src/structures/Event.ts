2763584016ed1fabde165f0371a867aa55abf9f4
defnot001 <marc.miller994@gmail.com>
2022-11-25 21:55:58 +0100

    removed comments

--- src/structures/Event.ts
+++ src/structures/Event.ts
@@ -1,16 +1,7 @@
 import type { ClientEvents } from 'discord.js';
-
-// export class Event<Key extends keyof ClientEvents> {
-//   constructor(
-//     public event: Key,
-//     public execute: (...args: ClientEvents[Key]) => unknown,
-//   ) {}
-// }
-
 export class Event<K extends keyof ClientEvents> {
   constructor(
     public event: K,
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     public execute: (...args: ClientEvents[K]) => void,
   ) {}
 }
