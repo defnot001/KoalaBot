8c10d700b4f3595a33744c601b166b0dba845025
defnot001 <marc.miller994@gmail.com>
2023-01-04 11:15:32 +0100

    added function to convert bytes to human readable

--- src/util/functions/helpers.ts
+++ src/util/functions/helpers.ts
@@ -1,22 +1,35 @@
 import type { ApplicationCommandOptionChoiceData } from 'discord.js';
 import { mcConfig } from '../../config/config';
 
-export const getServerChoices =
-  (): ApplicationCommandOptionChoiceData<string>[] => {
-    const choices = [];
+export function getServerChoices(): ApplicationCommandOptionChoiceData<string>[] {
+  const choices = [];
 
-    for (const server of Object.keys(mcConfig)) {
-      choices.push({ name: server, value: server });
-    }
+  for (const server of Object.keys(mcConfig)) {
+    choices.push({ name: server, value: server });
+  }
 
-    return choices;
-  };
+  return choices;
+}
 
-export const escapeMarkdown = (text: string): string => {
+export function escapeMarkdown(text: string): string {
   const unescaped = text.replace(/\\(\*|_|`|~|\\)/g, '$1');
   return unescaped.replace(/(\*|_|`|~|\\)/g, '\\$1');
-};
+}
 
-export const includesUndefined = <T>(array: (T | undefined)[]) => {
+export function includesUndefined<T>(array: (T | undefined)[]) {
   return array.includes(undefined);
-};
+}
+
+export function humanReadableSize(bytes: number): string {
+  if (bytes < 1024) {
+    return bytes + ' bytes';
+  } else if (bytes < 1024 ** 2) {
+    return (bytes / 1024).toFixed(1) + ' KB';
+  } else if (bytes < 1024 ** 3) {
+    return (bytes / 1024 ** 2).toFixed(1) + ' MB';
+  } else if (bytes < 1024 ** 4) {
+    return (bytes / 1024 ** 3).toFixed(1) + ' GB';
+  } else {
+    return (bytes / 1024 ** 4).toFixed(1) + ' TB';
+  }
+}
